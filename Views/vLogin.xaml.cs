953104b62a9b475a59931d2bb3888f569067f049
master <master@SMLAB304PC03_24>
2024-06-11 21:40:13 -0500

    vectores

--- Views/vLogin.xaml.cs
+++ Views/vLogin.xaml.cs
@@ -6,21 +6,35 @@ public partial class vLogin : ContentPage
 	{
 		InitializeComponent();
 	}
-    string user;
-    string password;
+    //string user;
+    //string password;
+
+    int[] vector = new int[5]; // declara un vector de 5 elementos de tipo entero
+
+    // inicializa el vector con valores
+
     public vLogin(string usuario, string contrasena)
     {
         InitializeComponent();
-        user = usuario;
-        password = contrasena;
+        //user = usuario;
+        // password = contrasena;
+        vector[0] = new Usuario("");
+        vector[1] = 2;
+        vector[2] = 3;
+        vector[3] = 4;
+        vector[4] = 5;
+        foreach (int elemento in vector)
+        {
+            Console.WriteLine(elemento);
+        }
     }
   
 
     private void btnInicio_Clicked(object sender, EventArgs e)
     {
-        if (user == txtUsuario.Text && password== txtContraseña.Text)
+        if (vector == txtUsuario.Text && password== txtContraseña.Text)
         {
-            Navigation.PushAsync(new vHome(user));
+            Navigation.PushAsync(new vHome(vector));
         }
         else
         {
